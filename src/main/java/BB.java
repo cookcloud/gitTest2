59418fa0097a37802a12749c711f3377fac45368
cookcloud <cookcloud@aliyun.com>
2019-04-25 14:27:43 +0800

    stash冲突解决

--- src/main/java/BB.java
+++ src/main/java/BB.java
@@ -2,5 +2,6 @@ public class BB {
     public static void main(String[] args) {
         System.out.println("stash:文件新增");
         System.out.println("stash:github文件修改");
+        System.out.println("stash:本地文件修改");
     }
 }
